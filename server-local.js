a238aa288238647a4f113db95e955e893fc920c6
uzregxurshid <uzregxurshid@gmail.com>
2022-09-28 00:56:16 +0500

    fix

--- server-local.js
+++ server-local.js
@@ -2,5 +2,5 @@
 
 const app = require('./express/server');
 
-app.listen(4000, () => console.log('Local app listening on port 4000!'));
+app.listen(process.env.PORT, () => console.log('Local app listening on port 4000!'));
 
